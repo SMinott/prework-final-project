398385e095db6ff83dbbdce899e0b8d97825b74b
Susan Minott <susan_minott@yahoo.com>
2022-04-15 12:37:05 -0400

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,6 @@
+/*Create an h2 element*/
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+
+/*Add h2 elemnet to body*/
+document.querySelector("body").appendChild(h2);
